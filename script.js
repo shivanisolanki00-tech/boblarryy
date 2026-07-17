10c103386038385337a6fd644fe9f073579ba9c2
shivanisolanki00-tech <shivanisolanki00@gmail.com>
2026-07-17 23:24:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function startJourney(){
 
-
 let sections = [
 "story",
 "memory",
@@ -8,129 +7,72 @@ let sections = [
 "birthday"
 ];
 
-
 sections.forEach(function(section,index){
 
-
 setTimeout(function(){
-
 
 document.getElementById(section).classList.add("show");
 
-
-},1500*(index+1));
-
+},1000*(index+1));
 
 });
-
 
 
 setTimeout(function(){
 
-
 document.getElementById("story").scrollIntoView({
-
 behavior:"smooth"
-
 });
 
-
-},1600);
-
-
+},1200);
 
 }
-
-
 
 
 
 function blowCandle(){
 
+let flame = document.querySelector(".flame");
 
-
-let flame=document.querySelector(".flame");
-
-
+if(flame){
 flame.style.display="none";
-
-
+}
 
 createConfetti();
 
-
-
-setTimeout(function(){
-
-
-alert("Your wish has been sent to the universe ✨❤️");
-
-
-},500);
-
-
-
 }
-
-
-
-
 
 
 
 function createConfetti(){
 
+for(let i=0;i<120;i++){
 
-let container=document.getElementById("confetti");
+let confetti=document.createElement("div");
 
+confetti.className="confetti-piece";
 
+confetti.style.left=Math.random()*100+"vw";
 
-for(let i=0;i<150;i++){
+confetti.style.top="-20px";
 
-
-let piece=document.createElement("div");
-
-
-piece.className="confetti-piece";
-
-
-
-piece.style.left=Math.random()*100+"vw";
-
-
-piece.style.top="-20px";
-
-
-
-piece.style.backgroundColor=
+confetti.style.background=
 [
-"#ff4d6d",
-"#ffd166",
-"#06d6a0",
-"#118ab2",
-"#ffffff",
-"#c77dff"
-]
-[Math.floor(Math.random()*6)];
+"pink",
+"yellow",
+"white",
+"violet",
+"skyblue"
+][Math.floor(Math.random()*5)];
 
 
-
-piece.style.animationDelay=
-Math.random()*2+"s";
-
+confetti.style.animationDuration=
+(2+Math.random()*3)+"s";
 
 
-piece.style.transform=
-"rotate("+Math.random()*360+"deg)";
-
-
-
-container.appendChild(piece);
-
+document.body.appendChild(confetti);
 
 
 }
-
-
 
 }
