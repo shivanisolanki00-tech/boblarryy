b47a1fab5e480e5375fe83bd600941f46f0dd898
shivanisolanki00-tech <shivanisolanki00@gmail.com>
2026-07-17 23:13:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// Story navigation handled through HTML links
+function showStory(){
+
+let sections = [
+    "story",
+    "memories",
+    "letters",
+    "final"
+];
+
+
+sections.forEach(function(id, index){
+
+    setTimeout(function(){
+
+        document.getElementById(id).classList.add("show");
+
+    }, (index + 1) * 1500);
+
+});
+
+
+window.scrollTo({
+
+top: window.innerHeight,
+
+behavior:"smooth"
+
+});
+
+}
